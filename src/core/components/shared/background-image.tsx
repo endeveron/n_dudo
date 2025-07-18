e73ca2fd2380f18dc7f669862ab4bd78b5ee8757
endeveron <endeveron@gmail.com>
2025-07-18 15:09:12 +0300

    style - add placeholder for bg.jpg

--- src/core/components/shared/background-image.tsx
+++ src/core/components/shared/background-image.tsx
@@ -4,6 +4,9 @@ import Image from 'next/image';
 
 import AnimatedAppear from '@/core/components/shared/animated-appear';
 
+const imgPlaceholder =
+  'data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAYGBgYGBgYHBwYJCgkKCQ0MCwsMDRQODw4PDhQfExYTExYTHxshGxkbIRsxJiIiJjE4Ly0vOEQ9PURWUVZwcJYBBgYGBgYGBgcHBgkKCQoJDQwLCwwNFA4PDg8OFB8TFhMTFhMfGyEbGRshGzEmIiImMTgvLS84RD09RFZRVnBwlv/CABEIAAYACgMBEQACEQEDEQH/xAAVAAEBAAAAAAAAAAAAAAAAAAADBv/aAAgBAQAAAACXX//EABUBAQEAAAAAAAAAAAAAAAAAAAIF/9oACAECEAAAAIS//8QAFQEBAQAAAAAAAAAAAAAAAAAAAAb/2gAIAQMQAAAApX//xAAfEAACAgEEAwAAAAAAAAAAAAACAwESBAAFBiFSVNL/2gAIAQEAAT8AxMrE2RLmgzKTKVsMYQRRFjmvmPU170HPAoFp3OZr7jPrX//EABwRAQACAQUAAAAAAAAAAAAAAAEAAhEDFDFSkv/aAAgBAgEBPwC5fUuDYc85CbN7V8k//8QAGxEAAgIDAQAAAAAAAAAAAAAAAQIAAxESISL/2gAIAQMBAT8AqRXGtaAd5kwlgT5Wf//Z';
+
 const BackgroundImage = () => {
   return (
     <AnimatedAppear className="absolute inset-0 bg-main-image-background">
@@ -14,6 +17,8 @@ const BackgroundImage = () => {
         fill
         priority
         quality={100}
+        placeholder="blur"
+        blurDataURL={imgPlaceholder}
       />
     </AnimatedAppear>
   );
